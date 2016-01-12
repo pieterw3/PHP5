dbf50738c20bb276e53b616165c05c0f7e4b8e02
pieterw3 <pieter_willemse@msn.com>
2016-01-13 00:07:28 +0100

    learning php5 filters

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include('header.php') ?>
+
+<div class="container">
+<div class="row">
+<div class="col-md-3"><h1>PHP 5 FILTERS</h1></div>
+<div class="col-md-6">
+<?php
+
+//php5 filters printed
+echo '<pre>';
+print_r(filter_list());
+echo '</pre>';
+
+
+
+//http://phphulp.jorendewit.nl/view/24/4/
+?>
+
+ </div>
+ 
+<div class="col-md-3"><h1></h1></div></div>
+ 
+ </div>
+ <?php include('footer.php')?>
